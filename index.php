133217d1be6768e83bc14253156a55ff6a508d74
Chantal Thomas <thomas.chantala@gmail.com>
2018-09-17 22:03:34 -0400

    adding random quote

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-    $quotes = [
-        0 => 'Even if you&#39;re on the right track, you&#39;ll get run over if you just sit there',
-        1 => 'Trust yourself. You know more than you think you do',
-        2 => 'You&#39;re going to fall down, but the world doesn&#39;t care how many times you fall down, as loong as it&#39;s one fewer than the number of times you get back up'
-    ]
+     $quotes = [
+            0 => 'Even if you&#39;re on the right track, you&#39;ll get run over if you just sit there',
+            1 => 'Trust yourself. You know more than you think you do',
+            2 => 'You&#39;re going to fall down, but the world doesn&#39;t care how many times you fall down, as loong as it&#39;s one fewer than the number of times you get back up'
+        ]
 ?>
 
 <!DOCTYPE html>
@@ -34,7 +34,7 @@
 			</div>
 		<h2>Random Quote</h2>
 		<blockquote>
-		<?php echo $quotes[0]; ?>
+		<?php echo $quotes[rand(0,2)]; ?>
 		</blockquote>
 
 	</div>
